4ee0d4da28a0043703c29fd2616704a93b91cf44
SenthilArunachalam <apraman1977@gmail.com>
2025-05-19 22:53:08 +0530

    Success

--- calculator/mathapp/views.py
+++ calculator/mathapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+def calculate_power(request):
+    power = None
+    i = ''
+    r = ''
+    
+    if request.method == 'POST':
+        try:
+            i = float(request.POST.get('intensity', 0))
+            r = float(request.POST.get('resistance', 0))
+            power = i ** 2 * r
+        except (ValueError, TypeError):
+            power = "Invalid input"
+
+    context = {
+        'i': i,
+        'r': r,
+        'power': power
+    }
+
+    return render(request, 'power_calc.html', context)
+ 
